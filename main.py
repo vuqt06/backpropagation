eddd64ae5301351acde21ce1f118577098b543c2
vuqt06 <vuqt06@gmail.com>
2024-02-14 16:25:50 -0500

    Add more operations and start building neurons

--- main.py
+++ main.py
@@ -8,24 +8,48 @@ class Value:
         self._op = _op
     
     def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, (self, other), '+')
 
         def _backward():
-            self.grad = 1 * out.grad
-            other.grad = 1 * out.grad
+            self.grad += 1 * out.grad
+            other.grad += 1 * out.grad
         out.backward = _backward
 
         return out
     
     def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, (self, other), '*')
 
         def _backward():
-            self.grad = other.data * out.grad
-            other.grad = self.data * out.grad
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
         out.backward = _backward
 
         return out
+    
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "Power must be a scalar"
+        out = Value(self.data ** other, (self,), f'**{other}')
+
+        def _backward():
+            self.grad += other * (self.data ** (other - 1)) * out.grad
+        out.backward = _backward
+
+        return out
+    
+    def __rmul__(self, other): # other * self
+        return self * other
+    
+    def __truediv__(self, other):
+        return self * (other ** -1)
+    
+    def __neg__(self):
+        return -1 * self
+    
+    def __sub__(self, other):
+        return self + (-other)
     
     def tanh(self):
         x = self.data
@@ -33,7 +57,32 @@ class Value:
         out = Value(t, (self,), 'tanh')
 
         def _backward():
-            self.grad = (1 - t**2) * out.grad
+            self.grad += (1 - t**2) * out.grad
         out.backward = _backward
 
         return out
+
+    def exp(self):
+        x = self.data
+        out = Value(math.exp(x), (self,), 'exp')
+
+        def _backward():
+            self.grad += out.data * out.grad
+        out.backward = _backward
+
+        return out
+
+    def backward(self):
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+        self.grad = 1
+        for v in reversed(topo):
+            v.backward()
+        return self.grad
---
+++ nn.py
@@ -0,0 +1,28 @@
+class Neuron:
+    def __init__(self, nin):
+        self.w = [Value(random.uniform(-1, 1)) for _ in range(nin)]
+        self.b = Value(random.uniform(-1, 1))
+
+    def __call__(self, x):
+        # w*x + b
+        act = sum(wi*xi for wi, xi in zip(self.w, x)) + self.b
+        return act.tanh()
+    
+
+class Layer:
+    def __init__(self, nin, nout):
+        self.neurons = [Neuron(nin) for _ in range(nout)]
+
+    def __call__(self, x):
+        outs = [neuron(x) for neuron in self.neurons]
+        return outs[0] if len(outs) == 1 else outs
+    
+class MLP:
+    def __init__(self, nin, nouts):
+        sz = [nin] + nouts
+        self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(sz)-1)]
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
