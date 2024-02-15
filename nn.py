839a386b8b97fe8f02fa8e69a025ff7ef633e553
vuqt06 <vuqt06@gmail.com>
2024-02-15 17:40:36 -0500

    Add parameters

--- nn.py
+++ nn.py
@@ -3,6 +3,9 @@ class Neuron:
         self.w = [Value(random.uniform(-1, 1)) for _ in range(nin)]
         self.b = Value(random.uniform(-1, 1))
 
+    def parameters(self):
+        return self.w + [self.b]
+
     def __call__(self, x):
         # w*x + b
         act = sum(wi*xi for wi, xi in zip(self.w, x)) + self.b
@@ -12,6 +15,9 @@ class Neuron:
 class Layer:
     def __init__(self, nin, nout):
         self.neurons = [Neuron(nin) for _ in range(nout)]
+    
+    def parameters(self):
+        return [p for neuron in self.neurons for p in neuron.parameters()]
 
     def __call__(self, x):
         outs = [neuron(x) for neuron in self.neurons]
@@ -21,6 +27,9 @@ class MLP:
     def __init__(self, nin, nouts):
         sz = [nin] + nouts
         self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(sz)-1)]
+
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
 
     def __call__(self, x):
         for layer in self.layers:
